1a6f26d65c47a169873baf6298894cafe56de991
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-07-28 14:22:47 -0300

    Conhecendo os Models

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_28_160722) do
+ActiveRecord::Schema.define(version: 2023_07_28_165910) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
